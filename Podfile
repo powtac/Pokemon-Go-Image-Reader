2e5a97701219c1dd02d1e904a10afc13bbb684bd
Simon Brüchner <powtac@gmx.de>
2017-01-01 21:31:58 +0100

    Initial commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ocr01' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ocr01
+  pod 'TesseractOCRiOS', '4.0.0'
+  pod "Fusuma"
+  pod 'GoogleAPIClientForREST/Sheets', '~> 1.1.1'
+  pod 'GTMOAuth2', '~> 1.1.4'
+end
---
+++ ocr01/ViewController.swift
@@ -0,0 +1,113 @@
+
+
+import UIKit
+import Fusuma
+import TesseractOCR
+
+class ViewController: UIViewController, FusumaDelegate {
+    
+ // @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+ // @IBOutlet weak var showButton: UIButton!
+    @IBOutlet weak var showButton: UIButton!
+    
+ // @IBOutlet weak var fileUrlLabel: UILabel!
+    @IBOutlet weak var fileUrlLabel: UILabel!
+    
+    @IBOutlet weak var result: UITextView!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        showButton.layer.cornerRadius = 2.0
+        self.fileUrlLabel.text = ""
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func showButtonPressed(_ sender: AnyObject) {
+        // Show Fusuma
+        let fusuma = FusumaViewController()
+        
+        fusumaCropImage = false
+        
+        fusuma.delegate = self
+        fusuma.cropHeightRatio = 0.6
+        
+        self.present(fusuma, animated: true, completion: nil)
+    }
+    
+    // MARK: FusumaDelegate Protocol
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+
+        //        switch source {
+//        case .camera:
+//            print("Image captured from Camera")
+//        case .library:
+//            print("Image selected from Camera Roll")
+//        default:
+//            print("Image selected")
+//        }
+        
+        imageView.image = image
+        
+        
+        let tesseract:G8Tesseract = G8Tesseract(language:"eng");
+        //tesseract.language = "eng+ita";
+        // tesseract.delegate = self;
+        tesseract.charWhitelist = "-/.,ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzßÖ0123456789";
+        tesseract.image = image
+        tesseract.recognize();
+        
+        print(tesseract.recognizedText);
+        result.text = tesseract.recognizedText;
+    }
+    
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+        print("video completed and output to file: \(fileURL)")
+        self.fileUrlLabel.text = "file output to: \(fileURL.absoluteString)"
+    }
+    
+    func fusumaDismissedWithImage(_ image: UIImage, source: FusumaMode) {
+//        switch source {
+//        case .camera:
+//            print("Called just after dismissed FusumaViewController using Camera")
+//        case .library:
+//            print("Called just after dismissed FusumaViewController using Camera Roll")
+//        default:
+//            print("Called just after dismissed FusumaViewController")
+//        }
+    }
+    
+    func fusumaCameraRollUnauthorized() {
+        
+        print("Camera roll unauthorized")
+        
+        let alert = UIAlertController(title: "Access Requested", message: "Saving image needs to access your photo album", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { (action) -> Void in
+            
+            if let url = URL(string:UIApplicationOpenSettingsURLString) {
+                UIApplication.shared.openURL(url)
+            }
+            
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) -> Void in
+            
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func fusumaClosed() {
+        // print("Called when the close button is pressed")
+    }
+    
+}
